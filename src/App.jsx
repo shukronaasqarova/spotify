13a309128f9ee96fea9fb499beba849d8b16e990
Shukrona <shukronaasqarova94@gmail.com>
2024-11-02 03:13:37 +0500

    home

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Details from './pages/Details'
+import Likes from './pages/Likes'
+import http from './axios'
+import MainLayout from './layout/MainLayout'
+
+function App() {
+
+  useEffect(() => {
+    http.get('featured-playlists')
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  })
+  return (
+    <div>
+      <Routes>
+          <Route path='/' element={<MainLayout><Home></Home></MainLayout>}></Route>
+          <Route path='/likes' element={<MainLayout><Likes></Likes></MainLayout>}></Route>
+          <Route path='/details/:id' element={<MainLayout><Details></Details></MainLayout>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
